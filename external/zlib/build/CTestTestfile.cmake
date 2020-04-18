9d2500c40febe315817fe93feaa5395e47c1ab65
Jeff <dagestanec5550@gmail.com>
2020-04-18 15:55:19 -0400

    Revert "added zlib build folder"
    This reverts commit eddcc83d74d65ac73ad83c10ebdc9921dd00e441.

--- external/zlib/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/DevLOL/why am i doing this agian/vita3k/external/zlib
-# Build directory: E:/DevLOL/why am i doing this agian/vita3k/external/zlib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
-set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "E:/DevLOL/why am i doing this agian/vita3k/external/zlib/CMakeLists.txt;235;add_test;E:/DevLOL/why am i doing this agian/vita3k/external/zlib/CMakeLists.txt;0;")
